3eb4a72c87a36b1c36cbcad168907a036f6a8e31
Jerome1618 <89399088+Jerome1618@users.noreply.github.com>
2026-03-31 15:56:11 +0200

    Update server.js

--- server.js
+++ server.js
@@ -8,6 +8,10 @@ app.use(express.json());
 app.use(cors());
 
 app.use((req, res, next) => {
+  if (req.path === "/") {
+    return next();
+  }
+
   const key = req.headers["x-api-key"];
   if (key !== process.env.API_KEY) {
     return res.status(401).json({ error: "Unauthorized" });
